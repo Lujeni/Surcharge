32a5eca3bd53f33d4b8b5e41be80bcad995c6271
Thebault Julien <thebault.julien@live.fr>
2013-03-16 02:17:51 +0100

    setup package

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+import overload
+
+setup(
+    name=overload.__name__,
+    version=overload.__version__,
+    packages=find_packages(),
+    author=overload.__author__,
+    author_email="julien@thebault.co",
+    description="Overload is a tool for benchmarking your web serve",
+    long_description=open('README.md').read(),
+    install_requires=['requests', 'gevent'],
+    include_package_data=True,
+    url='https://github.com/Lujeni/Overload',
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: French",
+        "Operating System :: Unix",
+        "Topic :: System :: Networking",
+    ],
+    entry_points={
+        'console_scripts': [
+            'overload = overload.overload:main',
+        ],
+    },
+)
